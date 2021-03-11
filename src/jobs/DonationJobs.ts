aee43e24cd855a98f872357de21789c808018c86
David Munguia <david.munguia@viaro.net>
2021-03-11 07:39:12 -0600

    cron job added

---
+++ src/jobs/DonationJobs.ts
@@ -0,0 +1,32 @@
+import cron from 'cron';
+import { DonationModel } from "../models/db/DonationModel";
+const donationCron = new cron.CronJob("* * 0-23/2 * * *", async () => {
+
+    const donation = new DonationModel({
+        Frequency: 1,
+        Amount: 1000,
+        CardType: "VISA",
+        CardNumber: "4012888888881881",
+        CVV: "123",
+        NameCard: "David Munguia",
+        ExpirationDate: "01/23",
+        Email: "damunguia@protonmail.com",
+        Country: "GT",
+        Address1: "some random address",
+        Address2: "some random address",
+        City: "Quiche",
+        State: "Quiche",
+        ZipCode: "01064",
+        AgencyId: "6047f398cf1444456496f106",
+        Date: "01-01-2020",
+        UserId: "6047f398cf1444456496f106"
+
+    });
+    const result = await donation.save();
+    console.log(`Donation saved: ${result.id}`);
+
+});
+
+export const StartJobs=()=>{
+    donationCron.start();
+};
